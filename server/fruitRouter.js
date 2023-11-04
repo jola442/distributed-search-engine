60d9b258ff1365be5fc3e760139d3e870cd30987
Ayomide <ayomideawonaya7@gmail.com>
2023-11-04 03:11:28 -0400

    id

--- server/fruitRouter.js
+++ server/fruitRouter.js
@@ -7,9 +7,56 @@ router.get("/", respondWithFruits);
 router.get("/:id", respondWithFruit);
 
 //send back JSON with {_id, name, url, title, score, pr}
-async function respondWithFruit(req, res){
-
-}
+// Send back JSON with {_id, name, url, title, score, pr, wordFrequency}
+async function respondWithFruit(req, res) {
+    try {
+      const id = req.params.id;
+      const page = await Page.findOne({ _id: id, type: 'fruits' });
+      if (page) {
+        const {
+          _id,
+          url,
+          content: { title, pText },
+          pr,
+        } = page;
+  
+        // Calculate word frequency
+        const words = pText.split(/\s+/);
+        const wordFrequency = {};
+  
+        for (const word of words) {
+          if (wordFrequency[word]) {
+            wordFrequency[word]++;
+          } else {
+            wordFrequency[word] = 1;
+          }
+        }
+  
+        // Replace the placeholders with actual data
+        const name = "Fruit Name"; // Replace with the actual name.
+        const score = 4.5; // Replace with the actual score.
+  
+        // Create the response object
+        const response = {
+          _id,
+          name,
+          url,
+          title,
+          score,
+          pr,
+          wordFrequency,
+        };
+  
+        res.json(response);
+      } else {
+        res.status(404).json({ message: 'Fruit page not found' });
+      }
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ message: 'Internal Server Error' });
+    }
+  }
+  
 
 //send back JSON with {_id, url, title, pr, incomingLinks, outgoingLinks, wordFrequency}
 async function respondWithFruits(req, res){
